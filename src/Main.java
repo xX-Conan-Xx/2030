7093f7280dcd7b4fd17ea209ad8c969405b1b41e
xX-Conan-Xx <61041807+xX-Conan-Xx@users.noreply.github.com>
2020-09-28 14:18:34 +0800

    Level 3 final version

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,8 @@ public class Main {
         //System.out.println(new ArriveEvent(new Customer(1, 0.5), Arrays.asList(new Server(1, true, false, 0))).execute().execute());
         //System.out.println(new ArriveEvent(new Customer(2, 0.6), Arrays.asList(new Server(1, false, false, 1.0))).execute().execute().execute());
         //System.out.println(new ArriveEvent(new Customer(3, 0.6), Arrays.asList(new Server(1, false, true, 1.0))).execute());
+        //System.out.println(new ArriveEvent(new Customer(1, 0.5), Arrays.asList(new Server(1, false, false, 0), new Server(2, true, false, 0))).execute().execute());
+        //System.out.println(new ArriveEvent(new Customer(1, 0.5), Arrays.asList(new Server(1, false, true, 5.0), new Server(2, false, false, 10.0))).execute().execute().execute());
+        //System.out.println(new ArriveEvent(new Customer(1, 0.5), Arrays.asList(new Server(1, false, true, 5.0), new Server(2, false, true, 10.0))).execute());
     }
 }
